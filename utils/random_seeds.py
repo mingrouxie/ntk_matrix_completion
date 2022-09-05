f853261f61a607b8f7fc63be581fe2dc04e6a4b6
mingrouxie <mingrou.xie97@gmail.com>
2022-09-05 16:08:43 -0400

    Added hyperopt seed

--- utils/random_seeds.py
+++ utils/random_seeds.py
@@ -4,6 +4,7 @@ MODEL_SEED = 5
 PACKAGE_LOADER_SEED = 5
 HYPPARAM_SEED = 5
 SUBSTRATE_PRIOR_SELECTION_SEED = 424956
+HYPEROPT_SEED = 5931
 
 # TODO: kept the same values as before, but they should be different numbers just in case... 
 
