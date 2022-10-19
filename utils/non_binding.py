a2cf063fe00da953cf05cd460ee376c1991f2e47
mingrouxie <mingrou.xie97@gmail.com>
2022-10-19 10:12:27 -0400

    added zero to non-binding treatment options

--- utils/non_binding.py
+++ utils/non_binding.py
@@ -1,23 +1,27 @@
 from enum import Enum, IntEnum
-
+import numpy as np
 
 class NonBinding(IntEnum):
     ROW_MEAN = 1
     SMALL_POS = 2
     LARGE_POS = 3
     MAX_PLUS = 4
+    ZERO = 5
 
 
 def fill_non_bind(mat, nb_type):
     if nb_type == NonBinding.ROW_MEAN:
         return mat.apply(lambda row: row.fillna(row.mean()), axis=1)
     elif nb_type == NonBinding.SMALL_POS:
-        return mat.apply(lambda row: row.fillna(1e-5), axis=1)
+        return np.nan_to_num(mat, nan=1e-5, posinf=1e-5, neginf=None)
     elif nb_type == NonBinding.LARGE_POS:
+        return np.nan_to_num(mat, nan=10, posinf=10, neginf=None)
         # return mat.apply(lambda row: row.fillna(5), axis=1)
-        return mat.apply(lambda row: row.fillna(10), axis=1)
+        # return mat.apply(lambda row: row.fillna(10), axis=1)
         # return ground_tmatruth.apply(lambda row: row.fillna(30), axis=1)
     elif nb_type == NonBinding.MAX_PLUS:
         return mat.apply(lambda row: row.fillna(row.max() * 1.01), axis=1)
+    elif nb_type == NonBinding.ZERO:
+        return np.nan_to_num(mat, nan=0.0, posinf=0.0, neginf=None)
     else:
-        raise Exception("Non-binding treatment unrecognised:", nb_type)
+        raise Exception("Non-binding treatment not recognised:", nb_type)
