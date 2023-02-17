71bbaeb04a32b65c7dbf59b0bb928b67d605acf0
mrx <mrx@eofe7.mit.edu>
2023-02-16 22:50:49 -0500

    230216 experiments

--- run_scripts/expts/230131_mt/README.md
+++ run_scripts/expts/230131_mt/README.md
@@ -1,2 +1,12 @@
 - run_1: multitasknncorr, cpu, 
 - run_2: multitasknnsep, cpu
+
+16 Feb 23
+Going to run everything at 1000 epochs because... yeah... 
+Input scaler: standard, energy_scaler: minmax, load_scaler: null but I think we need to scale it... 
+- run_3: full file, multitasknnsep, load single, cpu
+- run_4: full file, multitasknncorr, load single, cpu
+- run_5: full file, multitasknnsep_v2, load load, cpu
+- run_6: full file, multitasknnsep_v2, load load_norm, cpu
+- run_7: full file, multitasknncorr_v2, load load, cpu
+- run_8: full file, multitasknncorr_v2, load load_norm, cpu
---
+++ run_scripts/expts/230131_mt/run_8/run_8.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=run_8
+#SBATCH --output=run_8_%j.log 
+#SBATCH --partition=sched_mit_rafagb,sched_mit_hill,sched_opportunist
+#SBATCH --time=20:00:00                  # hh:mm:ss, alternatively 1-00:00 dd-hh:mm
+#SBATCH -n 12                           # cores. 8 for voronoi, 1 for dreiding. Doing 12 now and I think my code does nthread=5 for xgb
+#SBATCH -N 1                            # nodes. 1 for voronoi, 1 for dreiding
+#SBATCH --mem-per-cpu=5000              # mb. 3800 for voronoi, 1000 for dreiding
+
+# https://engaging-web.mit.edu/eofe-wiki/slurm/resources/ 
+
+source /home/mrx/.bashrc
+source /home/mrx/bin/miniconda3/bin/activate /home/mrx/bin/miniconda3/envs/ntk
+
+ROOT="/home/mrx/projects/affinity"
+SRC="${ROOT}/ntk_matrix_completion"
+
+python $SRC/models/multitask/train.py --config $SRC/run_scripts/expts/230131_mt/run_8/run_8.yml
+
+
+echo 'done'
