8c16eac80eafccb8a9c2cbb86cf82d2f28b42b19
mrx <mrx@eofe7.mit.edu>
2023-02-24 16:38:26 -0500

    documenting earlystopper

--- utils/early_stopping.py
+++ utils/early_stopping.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 class EarlyStopper:
-    '''From https://stackoverflow.com/questions/71998978/early-stopping-in-pytorch'''
+    '''From https://stackoverflow.com/questions/71998978/early-stopping-in-pytorch
+    patience: threshold for counter
+    min_delta: positive delta
+    counter: keep track of epochs with loss within a min_delta of the previous losses
+    min_validation_loss: updated with the lowest loss
+    '''
     def __init__(self, patience=1, min_delta=0):
         self.patience = patience
         self.min_delta = min_delta
@@ -12,7 +17,7 @@ class EarlyStopper:
         if validation_loss < self.min_validation_loss:
             self.min_validation_loss = validation_loss
             self.counter = 0
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
+        elif validation_loss > (self.min_validation_loss + self.min_delta): # if val_loss is within this delta then counter does not increase, allows for fluctuations if specified
             self.counter += 1
             if self.counter >= self.patience:
                 return True
