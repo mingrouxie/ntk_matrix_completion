798f399ca267558f7c0f56329aeaf702689f2134
mrx <mrx@eofe7.mit.edu>
2023-03-11 00:09:17 -0500

    np array and pytorch tensors allowed for loss now (hacky)

--- utils/loss.py
+++ utils/loss.py
@@ -5,33 +5,40 @@ from math import sqrt
 import torch.nn as nn
 import torch
 
+def arr_to_ten(arr):
+    '''Convert numpy arrays to torch Tensors'''
+    if type(arr) == np.ndarray:
+        return torch.Tensor(arr)    
+    if type(arr) == torch.Tensor:
+        return arr
+
 def masked_loss(y, pred, mask, loss_type='mse'):
     '''Masked regression loss for binding energies'''
+    y = arr_to_ten(y)
+    pred = arr_to_ten(pred)
+    loss = LOSS_TYPES[loss_type]() # TODO: is this best practice 
+
     if sum(mask) == 0:
         # all non-binding entries
         return torch.tensor([0.0], requires_grad=True)
     
-    if y.dim() == 1:
-        y = y.resize(y.shape[0], 1) # TODO: hardcoded
     y_masked = y[mask==1]
     pred_masked = pred[mask==1]
-    loss = LOSS_TYPES[loss_type]() # TODO: is this best practice 
     # loss = nn.MSELoss(reduction='mean')
 
-    if np.isnan(loss(y_masked, pred_masked).detach().cpu().numpy()):
-        print("[masked_mse_loss] nan present in loss")
+    # if np.isnan(loss(y_masked, pred_masked).detach().cpu().numpy()):
+    #     print("[masked_mse_loss] nan present in loss")
 
     return loss(y_masked, pred_masked)
 
 def mse_loss(y, pred):
     '''MSE loss'''
-    
-    if y.dim() == 1:
-        y = y.resize(y.shape[0], 1) # TODO: hardcoded
+    y = arr_to_ten(y)
+    pred = arr_to_ten(pred)
     loss = nn.MSELoss(reduction='mean')
 
-    if np.isnan(loss(y, pred).detach().cpu().numpy()):
-        print("[mse_loss] nan present in loss")
+    # if np.isnan(loss(y, pred).detach().cpu().numpy()):
+    #     print("[mse_loss] nan present in loss")
 
     return loss(y, pred)
 
@@ -49,39 +56,37 @@ class RMSELoss(nn.Module):
 
 def rmse_loss(y, pred):
     '''RMSE loss'''
-    
-    if y.dim() == 1:
-        y = y.resize(y.shape[0], 1) # TODO: hardcoded
+    y = arr_to_ten(y)
+    pred = arr_to_ten(pred)
     loss = RMSELoss()
 
-    if np.isnan(loss(y, pred).detach().cpu().numpy()):
-        print("[rmse_loss] nan present in loss")
+    # if np.isnan(loss(y, pred).detach().cpu().numpy()):
+    #     print("[rmse_loss] nan present in loss")
 
     return loss(y, pred)
 
 
 def mae_loss(y, pred):
     '''MAE loss'''
-
-    if y.dim() == 1:
-        y = y.resize(y.shape[0], 1) # TODO: hardcoded
+    y = arr_to_ten(y)
+    pred = arr_to_ten(pred)
     loss = nn.L1Loss()
 
-    if np.isnan(loss(y, pred).detach().cpu().numpy()):
-        print("[mae_loss] nan present in loss")
+    # if np.isnan(loss(y, pred).detach().cpu().numpy()):
+    #     print("[mae_loss] nan present in loss")
 
     return loss(y, pred)
 
 
 def cross_entropy_loss(y, pred):
-    if y.dim() == 1:
-        y = y.reshape(y.shape[0], 1) # TODO: hardcoded
+    y = arr_to_ten(y)
+    pred = arr_to_ten(pred)
     # proba = pred.softmax(dim=1)
     loss = nn.CrossEntropyLoss() 
 
     # if np.isnan(loss(y, proba).detach().cpu().numpy()):
-    if np.isnan(loss(y, pred).detach().cpu().numpy()):
-        print("[classifier] nan present in loss")
+    # if np.isnan(loss(y, pred).detach().cpu().numpy()):
+    #     print("[classifier] nan present in loss")
 
     # return loss(y, proba)
     return loss(y, pred)
@@ -98,15 +103,32 @@ def multitask_loss(y, y_preds, mask, loss_type='mse'):
         loading loss, energy loss
     '''
 
-    # print("[multitaskloss debug]", min(y_preds[0]), max(y_preds[0]), min(y_preds[1]), max(y_preds[1]))
+    # energy_y = torch.Tensor(y[:,0])
+    # energy_pred = torch.Tensor(y_preds[1])
+    # load_y = torch.Tensor(y[:,1:])
+    # load_pred = torch.Tensor(y_preds[0])
+    energy_y = y[:,0]
+    energy_pred = y_preds[1]
+    load_y = y[:,1:]
+    load_pred = y_preds[0]
+    
+    # reshape (view is only for Tensor, and resize is deprecated)
+    if energy_y.ndim == 1:
+        energy_y = energy_y.reshape(energy_y.shape[0], 1)
+        energy_pred = energy_pred.reshape(energy_pred.shape[0], 1)
+    
+    if load_y.ndim == 1:
+        load_y = load_y.reshape(load_y.shape[0], 1)
+        load_pred = load_pred.reshape(load_pred.shape[0], 1)
 
-    # TODO: hardcoded for loading of variable length 
-    energy_loss = masked_loss(y[:,0], y_preds[1], mask, loss_type=loss_type)
+    # print("[multitaskloss debug]", min(y_preds[0]), max(y_preds[0]), min(y_preds[1]), max(y_preds[1]))
+    energy_loss = masked_loss(energy_y, energy_pred, mask, loss_type=loss_type)
     # TODO: right now loss_type is not specified in train.py nor config file. Also, need separate loss_types for energy and for loading TODO TODO TODO 
-    if y[:,1:].shape[1] > 1:
-        load_loss = cross_entropy_loss(y[:,1:], y_preds[0])
+    if load_y.shape[1] > 1:
+        load_loss = cross_entropy_loss(load_y, load_pred)
     else:
-        load_loss = mse_loss(y[:,1:], y_preds[0])
+        load_loss = LOSS_TYPES[loss_type](load_y, load_pred)
+
     return load_loss, energy_loss 
 
 LOSS_TYPES = {
